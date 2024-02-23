997a6b76c34b4a6622ff3324a359a416b1b50e3b
weiyann <jona11105@gmail.com>
2024-02-23 16:45:06 +0800

    create authContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import "@/styles/styleguide.css";
+import { AuthProvider } from "@/context/authContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
